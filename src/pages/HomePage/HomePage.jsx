aa684590492a5e38c2d69c92aadae480a1fb8d39
kristyJinny <kristyjinny@gmail.com>
2024-04-08 00:30:15 +0900

    netlify.toml

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Banner from "./components/Banner/Banner";
+import PopularMovieSlider from "./components/PopularMovieSlide/PopularMovieSlider";
 
 // 1. 배너 => 인기 영화를 들고 와서, 첫 번째 아이템을 보여주자 -> 컴포넌트 만들기
 // 2. 인기 영화 -> API 호출
@@ -7,7 +8,10 @@ import Banner from "./components/Banner/Banner";
 // 4. 개볼 예정 영화
 
 export const HomePage = () => {
-  return <div data-page="HomePage">
-    <Banner/>
-  </div>;
+  return (
+    <div data-page="HomePage">
+      <Banner />
+      <PopularMovieSlider />
+    </div>
+  );
 };
---
+++ src/pages/HomePage/components/PopularMovieSlide/PopularMovieSlider.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+import { usePopularMoviesQuery } from "../../../../hooks/usePopularMovies";
+import Alert from "react-bootstrap/Alert";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+
+const responsive = {
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 8,
+    //partialVisibilityGutter: 40 // this is needed to tell the amount of px that should be visible.
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 2,
+    //partialVisibilityGutter: 30 // this is needed to tell the amount of px that should be visible.
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1,
+    //partialVisibilityGutter: 30 // this is needed to tell the amount of px that should be visible.
+  },
+};
+
+const PopularMovieSlider = () => {
+  const { data, isLoading, isError, error } = usePopularMoviesQuery();
+
+  if (isLoading) return <div>Loading...</div>;
+  if (isError) {
+    <Alert variant="danger">{error.message}</Alert>;
+  }
+
+  return (
+    <div>
+      <h3>PopularMovieSlider</h3>
+      <Carousel
+        infinite={true}
+        centerMode={true}
+        itemClass="movie-slider p-1"
+        containerClass="carousel-container"
+        responsive={responsive}
+
+        // swipeable={false}
+        // draggable={false}
+        // showDots={true}
+        // ssr={true}
+
+        // autoPlay={this.props.deviceType !== "mobile" ? true : false}
+        // autoPlaySpeed={1000}
+        // keyBoardControl={true}
+        // customTransition="all .5"
+        // transitionDuration={500}
+        // removeArrowOnDeviceType={["tablet", "mobile"]}
+        // deviceType={this.props.deviceType}
+        // dotListClass="custom-dot-list-style"
+      >
+        <div>Item 1</div>
+        <div>Item 2</div>
+        <div>Item 3</div>
+        <div>Item 4</div>
+      </Carousel>
+      ;
+    </div>
+  );
+};
+
+export default PopularMovieSlider;
