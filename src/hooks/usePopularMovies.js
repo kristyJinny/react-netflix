c9044bd826e75254a2ab2db4f9ddd6b0743466e3
kristyJinny <kristyjinny@gmail.com>
2024-04-07 12:55:11 +0900

    에러

--- src/hooks/usePopularMovies.js
+++ src/hooks/usePopularMovies.js
@@ -8,7 +8,7 @@ const fectchPopularMovies = () => {
 
 export const usePopularMoviesQuery = () => {
     return useQuery({
-        queryKey: ['move-popular'],
+        queryKey: ['movie-popular'],
         queryFn: fectchPopularMovies
     })
 }
